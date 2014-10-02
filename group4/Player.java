124b804f293cfd190a58d13b157b47b728f202c0
Andrew Kallem <ajk2214@columbia.edu>
2014-10-01 20:06:42 -0400

    Refactord Board class to make it a separate file with fuller methods

--- group4/Player.java
+++ group4/Player.java
@@ -5,6 +5,7 @@ import java.util.*;
 import offset.sim.Pair;
 import offset.sim.Point;
 import offset.sim.movePair;
+import offset.group4.Board;
 
 public class Player extends offset.sim.Player {
 	private int size = 32;
@@ -13,29 +14,8 @@ public class Player extends offset.sim.Player {
 		super(prin, idin);
 	}
 	
-	///////////////////////// PRIVATE METHODS /////////////////////////
-	// Light wrapper class for the board to make it easier to refer to specific locations
-	private class Board {
-		Point grid[];
-		
-		Board() {}
-		
-		Board(Point grid[]) {
-			this.grid = grid;
-		}
-		
-		public void update(Point grid[]) {
-			this.grid = grid;
-		}
-		
-		public Point get(int x, int y) {
-			return grid[x*size + y];
-		}
-		
-		public Point get(Point p) {
-			return this.get(p.x, p.y);
-		}
-	}
+	///////////////////////// PRIVATE CLASSES /////////////////////////
+
 	
 	///////////////////////// PRIVATE VARIABLES /////////////////////////
 	Board board;
@@ -43,7 +23,7 @@ public class Player extends offset.sim.Player {
 	
 	///////////////////////// SETUP /////////////////////////
 	public void init() {
-		board = new Board();
+		board = new Board(size);
 	}
 	
 	public movePair move(Point[] grid, Pair pr, Pair pr0, ArrayList<ArrayList> history) {
@@ -51,7 +31,7 @@ public class Player extends offset.sim.Player {
 			init();
 		
 		// Update the board with the latest grid.  Optimize later if need be based on history
-		board.update(grid);
+		board.setGrid(grid);
 		
 		movePair movepr = new movePair();
 		
@@ -61,19 +41,14 @@ public class Player extends offset.sim.Player {
 				int x = d - n;
 				int y = n;
 				
-				for (int c = 0; c < 4; c++) {
-					Point p = new Point();
-					p.x = x;
-					p.y = y;
-					
-					ArrayList<Point> validMoves = validMovesFrom(p);
+				for (int c = 0; c < 4; c++) {					
+					ArrayList<Point> validMoves = board.validMovesFrom(x, y, pr);
 					
 					// Return any valid moves from that point
 					if (!validMoves.isEmpty()) {
 						movepr.move = true;
-						movepr.src = board.get(validMoves.get(0));
-						movepr.target = board.get(p);
-						
+						movepr.src = board.get(validMoves.get(0).x, validMoves.get(0).y);
+						movepr.target = board.get(x, y);
 						
 						return movepr;
 					}
@@ -89,57 +64,5 @@ public class Player extends offset.sim.Player {
 		// Could not find a valid move
 		movepr.move = false;
 		return movepr;
-	}
-
-	///////////////////////// HELPER METHODS /////////////////////////
-	
-	// Given a point, returns an ArrayList of points that can be combined with it
-	// A point can validly be combined if it is at the proper offset for the player and has the same value
-	private ArrayList<Point> validMovesFrom(Point from) {
-		ArrayList<Point> validMoves = new ArrayList<Point>();
-		int p;
-		int q;
-		
-		if (from.value == 0)
-			return validMoves;
-			
-		for(int i = -1; i <= 1; i += 2) {
-			for(int j = -1; j <= 1; j += 2) {
-				for(int k = 0; k <= 1; k++) {
-					Point to = new Point();
-					p = k == 0 ? pr.p : pr.q;
-					q = k == 0 ? pr.q : pr.p;
-					
-					to.x = from.x + p*i;
-					to.y = from.y + q*j;
-					if (isMoveValid(from, to))
-						validMoves.add(to);
-				}				
-			}
-		}
-
-		return validMoves;
-	}
-	
-	private boolean isMoveValid(Point src, Point target) {
-		return (isInBounds(src) && isInBounds(target) &&
-				((Math.abs(src.x - target.x) == pr.p && Math.abs(src.y - target.y) == pr.q) || (Math.abs(src.x - target.x) == pr.q && Math.abs(src.y - target.y) == pr.p)) &&
-				board.get(src).value == board.get(target).value && board.get(src).value != 0);
-	}
-	
-	private boolean isMoveValid(movePair mp) {
-		return isMoveValid(mp.src, mp.target);
-	}
-	
-	private boolean isInBounds(Point point) {
-		return (point.x >= 0 && point.x < size && point.y >= 0 && point.y < size);
-	}
-	
-	private int distBetween(Point p1, Point p2) {
-		return (Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y));
-	}
-	
-	private double distFromCenter(Point p1) {
-		return (Math.abs(p1.x - (double) size/2) + Math.abs(p1.y - (double) size/2));
 	}
 }
