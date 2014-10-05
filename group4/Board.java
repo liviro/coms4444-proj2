b2387cba9dde53cdbb3ea9e330de0f299327e91c
Andrew Kallem <ajk2214@columbia.edu>
2014-10-05 11:35:04 -0400

    Modified Board class to use Coord and Move class.  Modified Player class to use Board appropriately.  Added recursive search to Player

--- group4/Board.java
+++ group4/Board.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 import offset.sim.Pair;
 import offset.sim.Point;
-import offset.sim.movePair;
+
+import offset.group4.Coord;
+import offset.group4.Move;
 
 public class Board {
 	ArrayList<Point> grid;
@@ -41,29 +43,32 @@ public class Board {
 	
 	// Updates the grid based on a move performed by a player
 	// Assumes that the move is valid
-	public void processMove(int xSrc, int ySrc, int xTarget, int yTarget, int player) {
+	public void processMove(int xSrc, int ySrc, int xTarget, int yTarget, int playerId) {
 		grid.get(xSrc*size + ySrc).value = 0;
 		grid.get(xSrc*size + ySrc).owner = -1;
 		grid.get(xTarget*size + yTarget).value *= 2;
-		grid.get(xTarget*size + yTarget).owner = player;
+		grid.get(xTarget*size + yTarget).owner = playerId;
 	}
 	
-	public void processMove(Point src, Point target, int player) {
+	public void processMove(Coord src, Coord target, int player) {
 		processMove(src.x, src.y, target.x, target.y, player);
 	}
 	
-	public void processMove(movePair mp, int player) {
-		if (mp.move)
-			processMove(mp.src, mp.target, player);
+	public void processMove(Move move) {
+		processMove(move.src, move.target, move.playerId);
 	}
 	
-	public Point get(int x, int y) {
-		return grid.get(x*size + y);
+	public Point getPoint(int x, int y) {
+		return new Point(grid.get(x*size + y));		// Return a copy so user cannot modify the board
+	}
+	
+	public Point getPoint(Coord c) {
+		return getPoint(c.x, c.y);
 	}
 	
 	// Given a point and an offset pair, returns an ArrayList of neighbors of that point that are on the board
-	public ArrayList<Point> neighborsOf(int x, int y, Pair pr) {
-		ArrayList<Point> neighbors = new ArrayList<Point>();
+	public ArrayList<Coord> neighborsOf(int x, int y, Pair pr) {
+		ArrayList<Coord> neighbors = new ArrayList<Coord>();
 		int p;
 		int q;
 
@@ -74,7 +79,7 @@ public class Board {
 			for(int i = -1; i <= 1; i += 2) {
 				for(int j = -1; j <= 1; j += 2) {
 					if (isInBounds(x + p*i, y + q*j))
-						neighbors.add(get(x + p*i, y + q*j));
+						neighbors.add(new Coord(x + p*i, y + q*j));
 				}
 			}
 		}
@@ -82,62 +87,62 @@ public class Board {
 		return neighbors;
 	}
 	
-	public ArrayList<Point> neighborsOf(Point p, Pair pr) {
-		return neighborsOf(p.x, p.y, pr);
+	public ArrayList<Coord> neighborsOf(Coord c, Pair pr) {
+		return neighborsOf(c.x, c.y, pr);
 	}
 	
-	public ArrayList<Point> validMovesFrom(int x, int y, Pair pr) {
-		ArrayList<Point> neighbors = neighborsOf(x, y, pr);
-		ArrayList<Point> validMoves = new ArrayList<Point>();
+	public ArrayList<Coord> validMovesFrom(int x, int y, Pair pr) {
+		ArrayList<Coord> neighbors = neighborsOf(x, y, pr);
+		ArrayList<Coord> validMoves = new ArrayList<Coord>();
 		
-		for (Point p : neighbors) {
-			if (isMoveValid(p.x, p.y, x, y, pr))
-				validMoves.add(new Point(p));
+		for (Coord c : neighbors) {
+			if (isMoveValid(c.x, c.y, x, y, pr))
+				validMoves.add(new Coord(c.x, c.y));
 		}
 		
 		return validMoves;
 	}
-	
-	public ArrayList<Point> validMovesFrom(Point p, Pair pr) {
-		return validMovesFrom(p.x, p.y, pr);
+
+	public ArrayList<Coord> validMovesFrom(Coord c, Pair pr) {
+		return validMovesFrom(c.x, c.y, pr);
 	}
 	
 	public boolean isInBounds(int x, int y) {
 		return (x >= 0 && x < this.size && y >= 0 && y < this.size);
 	}
 	
-	public boolean isInBounds(Point p) {
-		return isInBounds(p.x, p.y);
+	public boolean isInBounds(Coord c) {
+		return isInBounds(c.x, c.y);
 	}
 	
 	public boolean isMoveValid(int x1, int y1, int x2, int y2, Pair pr) {
 		return (isInBounds(x1, y1) && isInBounds(x2, y2) && 
 				((Math.abs(x1 - x2) == pr.p && Math.abs(y1 - y2) == pr.q) || (Math.abs(x1 - x2) == pr.q && Math.abs(y1 - y2) == pr.p)) &&
-				get(x1, y1).value == get(x2, y2).value &&
-				get(x1, y1).value > 0);
+				getPoint(x1, y1).value == getPoint(x2, y2).value &&
+				getPoint(x1, y1).value > 0);
 	}
 	
-	public boolean isMoveValid(Point src, Point target, Pair pr) {
+	public boolean isMoveValid(Coord src, Coord target, Pair pr) {
 		return isMoveValid(src.x, src.y, target.x, target.y, pr);
 	}
 	
-	public boolean isMoveValid(movePair mp, Pair pr) {
-		return isMoveValid(mp.src, mp.target, pr);
+	public boolean isMoveValid(Move move, Pair pr) {
+		return isMoveValid(move.src, move.target, pr);
 	}
 	
 	public int distBetween(int x1, int y1, int x2, int y2) {
 		return (Math.abs(x1 - x2) + Math.abs(y1 - y2));
 	}
 	
-	public int distBetween(Point p1, Point p2) {
-		return distBetween(p1.x, p1.y, p2.x, p2.y);
+	public int distBetween(Coord c1, Coord c2) {
+		return distBetween(c1.x, c1.y, c2.x, c2.y);
 	}
-	
+		
 	public double distFromCenter(int x, int y) {
 		return (Math.abs(x - (double) size/2) + Math.abs(y - (double) size/2));
 	}
 	
-	public double distFromCenter(Point p) {
-		return distFromCenter(p.x, p.y);
+	public double distFromCenter(Coord c) {
+		return distFromCenter(c.x, c.y);
 	}
 }
--- group4/Player.java
+++ group4/Player.java
@@ -6,6 +6,7 @@ import offset.sim.Pair;
 import offset.sim.Point;
 import offset.sim.movePair;
 import offset.group4.Board;
+import offset.group4.Move;
 
 public class Player extends offset.sim.Player {
 	private int size = 32;
@@ -26,21 +27,99 @@ public class Player extends offset.sim.Player {
 		
 	}
 	
+	///////////////////////// RECURSIVE MOVE SEARCH /////////////////////////
+	// Returns a shortest list of moves such that, after the moves, the point at coordinate c has value value
+	private ArrayList<Move> movesToMakeValue(Board board, Coord c, int value, Pair pair, int playerId) {
+		Point p = board.getPoint(c);
+		
+		if (p.value == value) {
+			return new ArrayList<Move>();				// No moves needed
+		} else if (p.value == 0 || p.value > value) {
+			return null;								// Impossible to get p to have value
+		} else {
+			Board newBoard = new Board(board);
+			ArrayList<Move> movesList = new ArrayList<Move>();
+			int currentValue = p.value;
+			
+			while (currentValue < value) {
+				ArrayList<Move> moves = movesToDoubleValue(newBoard, c, pair, playerId);
+				
+				if (moves != null) {
+					movesList.addAll(moves);
+					currentValue = newBoard.getPoint(c).value;
+				} else {
+					return null;
+				}
+			}
+			
+			return movesList;
+		}
+	}
+	
+	// Returns a shortest list of moves such that the value of the point at the given coordinate is doubled, i.e., one combination
+	// Includes the combination move itself as the last move in the list, with the given point receiving the coins.  (Of course, this final move can be reversed if desired)
+	// Applies the moves to the provided board.  Thus when called externally, a copy of the board should be provided
+	private ArrayList<Move> movesToDoubleValue(Board board, Coord c, Pair pair, int playerId) {
+		Point p = board.getPoint(c);
+		
+		if (p.value == 0) {
+			return null;			// Cannot increase the value if it is already zero
+		}
+		
+		int minMoves = Integer.MAX_VALUE;
+		ArrayList<Move> minMovesList = new ArrayList<Move>();
+		
+		ArrayList<Coord> neighbors = board.neighborsOf(c, pair);
+		
+		for (Coord n : neighbors) {
+			ArrayList<Move> moves = movesToMakeValue(board, n, p.value, pair, playerId);
+			
+			if (moves != null) {
+				moves.add(new Move(n, c, playerId));
+				if (moves.size() < minMoves) {
+					minMoves = moves.size();
+					minMovesList = moves;
+				}
+			}
+		}
+		
+		if (minMovesList.isEmpty()) {
+			return null;
+		} else {
+			for (Move move : minMovesList) {
+				board.processMove(move);
+			}
+			
+			return minMovesList;
+		}
+	}
+	
+	
+	///////////////////////// IMPLEMENT ABSTRACT CLASS MOVE METHOD /////////////////////////	
 	public movePair move(Point[] grid, Pair pr, Pair pr0, ArrayList<ArrayList> history) {
 		if (!didSetup) {
 			board = new Board(size, grid);
 			didSetup = true;
 		}
-		
 		// An element in the history ArrayList is itself an ArrayList where the first element is the player id and the second is a movePair (each of which must be cast)
 		// Keep our board up to date by processing the most recent moves made by opponent (faster than copying the whole grid again)
 		int i = history.size() - 1;
 		while (i >= 0 && (int) history.get(i).get(0) != id) {
 			int player = (int) history.get(i).get(0);
 			movePair mp = (movePair) history.get(i).get(1);
-			board.processMove(mp, player);
+			if (mp.move)
+				board.processMove(new Move(mp.src.x, mp.src.y, mp.target.x, mp.target.y, player));
 			i--;
 		}
+		
+		
+		Board newBoard = new Board(board);
+		ArrayList<Move> moves = movesToDoubleValue(newBoard, new Coord(6,8), pr, id);
+		
+		System.out.printf("%s\n\n\n", moves);
+		
+		
+		
 		
 		movePair movepr = new movePair();
 		
@@ -51,14 +130,14 @@ public class Player extends offset.sim.Player {
 				int y = n;
 				
 				for (int c = 0; c < 4; c++) {					
-					ArrayList<Point> validMoves = board.validMovesFrom(x, y, pr);
+					ArrayList<Coord> validMoves = board.validMovesFrom(x, y, pr);
 					
 					// Return any valid moves from that point
 					if (!validMoves.isEmpty()) {
 						movepr.move = true;
 						movepr.src = grid[validMoves.get(0).x*size + validMoves.get(0).y];
 						movepr.target = grid[x*size + y];
-						board.processMove(movepr, id);
+						board.processMove(new Move(movepr.src.x, movepr.src.y, movepr.target.x, movepr.target.y, id));
 						return movepr;
 					}
 					
@@ -72,7 +151,6 @@ public class Player extends offset.sim.Player {
 		
 		// Could not find a valid move
 		movepr.move = false;
-		board.processMove(movepr, id);
 		return movepr;
 	}
 }
